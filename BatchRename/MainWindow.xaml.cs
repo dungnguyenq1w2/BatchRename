5e051313eb4298a590de6bc36decfcbd301fac45
Nguyễn Tấn Dũng <dungnguyenq1w2@gmail.com>
2022-12-01 20:39:37 +0700

    add feature "save last window state": size of screen + position

--- BatchRename/MainWindow.xaml.cs
+++ BatchRename/MainWindow.xaml.cs
@@ -39,6 +39,16 @@ namespace BatchRename
 
         private void winMain_Loaded(object sender, RoutedEventArgs e)
         {
+            this.Top = Properties.Settings.Default.Top;
+            this.Left = Properties.Settings.Default.Left;
+            this.Height = Properties.Settings.Default.Height;
+            this.Width = Properties.Settings.Default.Width;
+            // Very quick and dirty - but it does the job
+            if (Properties.Settings.Default.Maximized)
+            {
+                WindowState = WindowState.Maximized;
+            }
+
             RenameRuleParserFactory.Instance().Register();
             BaseWindowFactory.Instance().Register();
 
@@ -797,6 +807,29 @@ namespace BatchRename
         private void lvRunRules_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                // Use the RestoreBounds as the current values will be 0, 0 and the size of the screen
+                Properties.Settings.Default.Top = RestoreBounds.Top;
+                Properties.Settings.Default.Left = RestoreBounds.Left;
+                Properties.Settings.Default.Height = RestoreBounds.Height;
+                Properties.Settings.Default.Width = RestoreBounds.Width;
+                Properties.Settings.Default.Maximized = true;
+            }
+            else
+            {
+                Properties.Settings.Default.Top = this.Top;
+                Properties.Settings.Default.Left = this.Left;
+                Properties.Settings.Default.Height = this.Height;
+                Properties.Settings.Default.Width = this.Width;
+                Properties.Settings.Default.Maximized = false;
+            }
+
+            Properties.Settings.Default.Save();
         }
     }
 }
